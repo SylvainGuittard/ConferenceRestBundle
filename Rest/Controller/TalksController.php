9bd2c4151ddf4a81859c4efb658e6a10847a28bd
Flo HUCK <flovntp@gmail.com>
2015-10-16 01:43:53 -0400

    Adding rest route to get talks for a specific speaker  talks/get/{speakerId}

--- Rest/Controller/TalksController.php
+++ Rest/Controller/TalksController.php
@@ -2,15 +2,20 @@
 
 namespace Ez\ConferenceRestBundle\Rest\Controller;
 
+use Ez\ConferenceRestBundle\Rest\ValueObjectVisitor\Talk;
 use Ez\ConferenceRestBundle\Rest\Values\Speakers;
-use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigResolver;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
 use eZ\Publish\Core\REST\Server\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Request;
 
 class TalkController extends BaseController
 {
+    /**
+     * Get the list of all Talks
+     * @return Talk
+     */
     public function getList( )
     {
         $rootLocation = $this->repository->getLocationService()->loadLocation( 2 );
@@ -31,4 +36,36 @@ class TalkController extends BaseController
 
         return new Talk( $hits, $contentType );
     }
+
+    /**
+     * Get the list of all Talks
+     * @return Talk
+     */
+    public function getListBySpeaker( Request $request )
+    {
+        $speakerId = $request->get( 'speakerId' );
+
+        $rootLocation = $this->repository->getLocationService()->loadLocation( 2 );
+        $query = new Query();
+        $query->filter = new Criterion\LogicalAnd(
+            array(
+                new Criterion\ContentTypeIdentifier( array('slot') ),
+                new Criterion\Visibility( Criterion\Visibility::VISIBLE ),
+                new Criterion\Subtree( $rootLocation->pathString ),
+                new Criterion\Field( 'speaker', Criterion\Operator::CONTAINS, $speakerId )
+            )
+        );
+        $query->sortClauses = array( new SortClause\Field( "slot", "starting_time", Query::SORT_DESC ));
+
+        $result = $this->repository->getSearchService()->findContent( $query )->searchHits;
+
+        $hits = $result;
+        $contentType = $this->repository->getContentTypeService()->loadContentTypeByIdentifier( 'slot' );
+
+        return new Talk( $hits, $contentType );
+    }
+
+
+
+
 }
