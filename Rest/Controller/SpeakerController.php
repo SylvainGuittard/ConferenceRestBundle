1072685cc63370e587080b799c9d1e82e3874a8e
SylvainGuittard <sylvain.guittard@gmail.com>
2015-10-20 20:29:44 +0200

    Fix Speaker Issue

--- Rest/Controller/SpeakerController.php
+++ Rest/Controller/SpeakerController.php
@@ -2,6 +2,7 @@
 
 namespace Ez\ConferenceRestBundle\Rest\Controller;
 
+use Ez\ConferenceRestBundle\Rest\Values\Speaker;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\Core\REST\Server\Controller as BaseController;
